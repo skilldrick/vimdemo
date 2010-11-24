7cc9b06572a15b9a2d1bcac6b9aeee3fc447cdc2
Skilldrick <skilldrick@gmail.com>
2010-11-24 23:12:42 +0000

    Can now draw arbitrary text on canvas with indents.

--- js/canvas.js
+++ js/canvas.js
@@ -8,6 +8,23 @@ VIM.canvasEngine = (function () {
     that.ctx.restore();
   }
 
+  /* Convenience functions to make the canvas API slightly nicer */
+
+  function font(weight, size, family) {
+    if (!weight) {
+      weight = 'normal';
+    }
+    if (!size) {
+      size = 12;
+    }
+    if (!family) {
+      family = 'monospace';
+    }
+
+    return weight + ' ' + parseInt(size, 10) + 'px ' + family;
+  }
+
+
   var that = {};
 
   that.init = function () {
@@ -15,6 +32,11 @@ VIM.canvasEngine = (function () {
     that.width = that.canvas.width;
     that.height = that.canvas.height;
     that.ctx = that.canvas.getContext('2d');
+    that.fontsize = 14;
+    that.fontwidth = that.fontsize / 1.75;
+    that.lineheight = that.fontsize * 1.4;
+    that.topmargin = 20;
+    that.leftmargin = 20;
     
     saveRestore(function (ctx) {
       ctx.fillStyle = 'black';
@@ -23,7 +45,24 @@ VIM.canvasEngine = (function () {
   };
 
   that.write = function (args) {
-    console.log(args.line, args.text);
+    //if more than one argument, call this function on each item
+    if (arguments.length > 1) { 
+      for (var i = 0; i < arguments.length; i++) {
+        that.write(arguments[i]);
+      }
+    }
+    if (!args.indent) {
+      args.indent = 0;
+    }
+    saveRestore(function (ctx) {
+      var xoffset = that.leftmargin + args.indent * that.fontwidth;
+      var yoffset = (args.line - 1) * that.lineheight + 
+                    that.topmargin;
+      ctx.fillStyle = 'white';
+      ctx.textBaseline = "top";
+      ctx.font = font('normal', that.fontsize);
+      ctx.fillText(args.text, xoffset, yoffset);
+    });
   };
 
   return that;
--- js/text.js
+++ js/text.js
@@ -6,10 +6,16 @@ VIM.textEngine = (function () {
   var that = {};
 
   that.init = function () {
-    VIM.canvasEngine.write({
-      line: 1,
-      text: 'Blah'
-    });
+    VIM.canvasEngine.write(
+      { line: 1, text: 'Hi.' },
+      { line: 2, text: 'I mean, Hello, world' },
+      { line: 3, text: "Yeah, that's what I meant to say." },
+      { line: 4, text: "    * * * * * * *"},
+      { line: 5, text: "   * * * * * * *", indent: 1  },
+      { line: 6, text: "  * * * * * * *", indent: 2  },
+      { line: 7, text: " * * * * * * *", indent: 3  },
+      { line: 8, text: "* * * * * * *", indent: 4  }
+    );
   };
 
   return that;
